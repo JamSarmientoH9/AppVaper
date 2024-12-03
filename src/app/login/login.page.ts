bf64df57b4f62e3d8e14f6eab1ff965c76b967e0
JamSarmientoH9 <jamkeatesissmith12@gmail.com>
2024-12-03 15:56:41 -0500

    Cambios validacion de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,12 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"],
 })
-export class LoginPage implements OnInit {
-  constructor() {}
+export class LoginPage {
+  username: string = "";
+  password: string = "";
+  users: any[] = [];
 
-  ngOnInit() {}
+  constructor(private router: Router) {}
+  onLogin() {
+    const correctUsername = "testUser";
+    const correctPassword = "testPassword";
+
+    if (
+      this.username === correctUsername &&
+      this.password === correctPassword
+    ) {
+      console.log("Inicio de sesión exitoso");
+      this.router.navigate(["/market"]);
+    } else {
+      console.log("Usuario o contraseña incorrectos");
+    }
+  }
+  onRegister() {
+    if (this.username && this.password) {
+      const newUser = { username: this.username, password: this.password };
+      this.users.push(newUser);
+      console.log("Registro exitoso: ", newUser);
+      this.username = "";
+      this.password = "";
+    } else {
+      console.log("Por favor, complete todos los campos");
+    }
+  }
+
+  onInputChange(event: any, inputName: string) {
+    console.log(
+      `Se ingresó texto en el campo ${inputName}:`,
+      event.target.value
+    );
+  }
+
+  onRememberMeChange(event: any) {
+    console.log('Checkbox "Remember Me" activado:', event.detail.checked);
+  }
 }
