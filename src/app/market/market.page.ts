f602e4bd33b82bc6d612e9690a62d81be2b263a5
JamSarmientoH9 <jamkeatesissmith12@gmail.com>
2024-12-05 21:38:41 -0500

    ggfas

--- src/app/market/market.page.ts
+++ src/app/market/market.page.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from "@angular/core";
 export class MarketPage implements OnInit {
   constructor() {}
 
-  yProduct(productName: string, productPrice: number) {
+  buyProduct(productName: string, productPrice: number) {
     console.log(
       `se compro el producto: ${productName}, al precio de: ${productPrice}`
     );
